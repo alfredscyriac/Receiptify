1f21b95d7dfc262a11e8c87b7acc52fc85b2d7e7
DilverSingh23 <dsinghh23@gmail.com>
2025-10-17 22:44:10 -0400

    add navbar to landing page

--- pages/index.js
+++ pages/index.js
@@ -3,16 +3,20 @@ import { ProblemSolution } from "./landing/ProblemSolution";
 import { Features } from "./landing/Features";
 import { HowItWorks } from "./landing/HowItWorks";
 import { CTA } from "./landing/CallToAction";
+import Navbar from "@/components/Auth/Navbar";
 
 const Index = () => {
   return (
-    <div className="min-h-screen">
-      <Hero />
-      <ProblemSolution />
-      <Features />
-      <HowItWorks />
-      <CTA />
-    </div>
+    <>
+      <div className="flex flex-col min-h-screen">
+        <Navbar />
+        <Hero />
+        <ProblemSolution />
+        <Features />
+        <HowItWorks />
+        <CTA />
+      </div>
+    </>
   );
 };
 
